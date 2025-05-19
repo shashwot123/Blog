6d8619b751f1518afe9d41c87d362bd9d53694e4
Shashwot Adhikari <shashwot123@gmail.com>
2025-05-19 15:33:05 +0545

    Added views to list the blogs and display a single blog post.

--- blog/urls.py
+++ blog/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from myblog import views
+from django.urls import include, path
 
 urlpatterns = [
-    path('myblog/', views.index_view),
+    path('myblog/', include("myblog.urls")),
     path('admin/', admin.site.urls),
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-def index_view(request):
-    return HttpResponse("Hello World")
+def post_list(request):
+    posts = Post.objects.filter(is_published=True).order_by("-published_at")
+    return render(request, "myblog/post_list.html", {"posts": posts})
+
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, "myblog/post_detail.html", {"post": post})
